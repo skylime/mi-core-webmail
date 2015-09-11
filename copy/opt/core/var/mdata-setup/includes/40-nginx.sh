6c08029e86c23a7c1b0d0c4b12dd623164837e64
Thomas Merkel <tm@core.io>
2015-09-11 13:37:34 +0200

    Don't require nginx_ssl because we could generate our own ssl certificate by default

--- copy/opt/core/var/mdata-setup/includes/40-nginx.sh
+++ copy/opt/core/var/mdata-setup/includes/40-nginx.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-# Configure nginx ssl certificate
+# Configure nginx ssl certificate and authentication
+
+host=$(hostname)
+cert_dir='/opt/local/etc/nginx/ssl/'
 
 # SSL
-mdata-get nginx_ssl > /opt/local/etc/nginx/ssl/nginx.pem
-chmod 400 /opt/local/etc/nginx/ssl/nginx.pem
+if mdata-get nginx_ssl 1>/dev/null 2>&1; then
+	mdata-get nginx_ssl > ${cert_dir}nginx.pem
+else
+	openssl req -newkey rsa:2048 -keyout ${cert_dir}nginx.key \
+				-out ${cert_dir}nginx.csr -nodes \
+				-subj "/C=DE/L=Raindbow City/O=Aperture Science/OU=Please use valid ssl certificate/CN=${host}"
+	openssl x509 -in ${cert_dir}nginx.csr -out ${cert_dir}nginx.crt -req -signkey ${cert_dir}nginx.key -days 128
+	cat ${cert_dir}nginx.crt ${cert_dir}nginx.key > ${cert_dir}nginx.pem
+fi
+chmod 400 ${cert_dir}nginx.pem
